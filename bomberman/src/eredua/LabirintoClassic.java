0ce0d10d3d4eaa2c2a24c529da9eb25d6ff99c55
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2025-02-17 19:40:22 +0100

    Update LabirintoClassic.java

--- bomberman/src/eredua/LabirintoClassic.java
+++ bomberman/src/eredua/LabirintoClassic.java
@@ -1,4 +1,28 @@
 package eredua;
+import java.until.Random;
 
 public class LabirintoClassic extends Labirintoa{
+  public LabirintoClassic(){
+    super();
+  }
+
+   @Override
+    protected char gelaxkaSortu(int i, int j) {
+        if (i % 2 == 1 && j % 2 == 1) {
+            return 'G'; //G: GOGORRA
+        } else {
+            double probabilitatea = random.nextDouble();
+            if (probabilitatea > 0.4) {
+                return 'B'; //B: BIGUNA
+            } else {
+                probabilitatea = random.nextDouble();
+                if (probabilitatea > 0.9 && etsaiKont < 6) {
+                    etsaiKont ++;
+                    return 'E'; //E: ETSAIA
+                } else {
+                    return 'H'; //H: HUTSIK
+                }
+            }
+        }
+    }
 }
