a7fa67e5e5e9fb84523a23ce891ff80e690cb78a
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-02-18 09:38:27 +0100

    Update Labirintoa.java

--- bomberman/src/eredua/Labirintoa.java
+++ bomberman/src/eredua/Labirintoa.java
@@ -1,4 +1,46 @@
 package eredua;
 
-public class Labirintoa {
+public abstract class Laberinto {
+	private Gelaxka[][] gelaxkak;
+	private int zabalera = 17;
+	private int altuera = 11;
+	
+	public Laberinto() {
+		this.gelaxkak = new Gelaxka[zabalera][altuera];
+	}
+	
+	public Gelaxka getGelaxka(int x, int y) {
+		return gelaxkak[x][y];
+	}
+
+	public Gelaxka[][] getGelaxkak() {
+		return gelaxkak;
+	}
+
+	public boolean bidePosizioa(int x, int y) {
+		return gelaxkak[x][y].bideaDago();
+	}
+
+	public void eguneratuGelaxka(int x, int y, String mota) {
+		switch (mota) {
+			case "hutsik":
+				gelaxkak[x][y] = new Gelaxka("hutsik", false);
+				break;
+			case "suntsiezina":
+				gelaxkak[x][y] = new Gelaxka("suntsiezina", true);
+				break;	
+			case "apurkorra":
+				gelaxkak[x][y] = new Gelaxka("apurkorra", false);
+				break;
+			case "bonba":
+				gelaxkak[x][y].bonbaJarri();
+				break;	
+			case "sua":
+				gelaxkak[x][y].suaJarri();
+				break;		
+		}
+	}
+
+
 }
+
