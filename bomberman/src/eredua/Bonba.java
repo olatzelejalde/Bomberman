9357f911e95a5892e2899f5d5ebd9ca145d9ba89
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-02-19 20:34:30 +0100

    Update Bonba.java

--- bomberman/src/eredua/Bonba.java
+++ bomberman/src/eredua/Bonba.java
@@ -1,4 +1,37 @@
-package eredua;
+package model;
 
-public class Bonba {
+public abstract class Bonba {
+	private int x, y;
+	private Laberinto laberinto;
+	
+	public Bonba(int x, int y, Laberinto laberinto) {
+		this.x = x;
+		this.y = y;
+		this.laberinto = laberinto;
+
+		itxaronEztanda();
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void itxaronEztanda() {
+		new Thread(() -> {
+			try {
+				Thread.sleep(3000); // 3s itxaron bonba eztanda egin arte
+				System.out.println("Bonba eztanda egin du!!");
+			}
+			catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+			
+		}).start();
+	}
+	
+	protected abstract void eztanda();
 }
