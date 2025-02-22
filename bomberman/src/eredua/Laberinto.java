0610adf61e4182011361e27c45e829a28e2ff344
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2025-02-22 14:15:26 +0100

    Update Laberinto.java

--- bomberman/src/eredua/Laberinto.java
+++ bomberman/src/eredua/Laberinto.java
@@ -3,25 +3,29 @@ package eredua;
 public abstract class Laberinto {
 	private static Laberinto nireLaberinto;
 	protected Gelaxka[][] matriz;
+	private Bomberman bomberman;
 	
 	protected Laberinto() {
 		this.matriz = new Gelaxka[11][17];
 		for (int i = 0; i < 11; i++) {
 			for (int j = 0; j < 17; j++) {
-				matriz[i][j] = new Gelaxka();
+				matriz[i][j] = new Gelaxka("hutsik");
 			}
 		}
 	}
 
 	public static Laberinto getNireLaberinto() {
 		if (nireLaberinto == null) {
-			nireLaberinto = new Laberinto();
+			nireLaberinto = new Classic();
 		}
 		return nireLaberinto;
 	}
 
 	public Gelaxka getGelaxkaPos(int x, int y) {
-		return matriz[x][y];
+		if (x >= 0 && x < 11 && y >= 0 && y < 17) {
+	        	return matriz[x][y];
+	    	}
+			return null;
 	}
 
 	public boolean bidePosizioa(int x, int y) {
@@ -31,16 +35,16 @@ public abstract class Laberinto {
 	public void eguneratuGelaxka(int x, int y, String mota) {
 		switch (mota) {
 			case "hutsik":
-				matriz[x][y] = new Gelaxka("hutsik", false);
+				matriz[x][y] = new Gelaxka("hutsik");
 				break;
 			case "gogorra":
-				matriz[x][y] = new Gelaxka("gogorra", true);
+				matriz[x][y] = new Gelaxka("gogorra");
 				break;	
 			case "biguna":
-				matriz[x][y] = new Gelaxka("biguna", false);
+				matriz[x][y] = new Gelaxka("biguna");
 				break;
 			case "bonba":
-				matriz[x][y].bonbaJarri();
+				bomberman.bonbaJarri(getNireLaberinto());
 				break;	
 			case "sua":
 				matriz[x][y].suaJarri();
