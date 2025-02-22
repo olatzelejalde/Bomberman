51f679238603a82e9a08fd15a3aa8af00199f33c
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-02-22 18:55:13 +0100

    Update Blokea.java

--- bomberman/src/eredua/Blokea.java
+++ bomberman/src/eredua/Blokea.java
@@ -1,22 +1,16 @@
 package eredua;
 
+public abstract class Bloke {
+	protected boolean suntsigarri;
+	
+	public Bloke(boolean suntsigarri) {
+		this.suntsigarri = suntsigarri;
+	}
+	
+	public boolean suntsigarriaDa() {
+		return suntsigarri;
+	}
+	
+	protected abstract void apurtu();
 
-public abstract class Blokea {
-    protected boolean suntsiezina;
-    protected boolean apurtuta;
-
-    public Blokea(boolean suntsiezina) {
-        this.suntsiezina = suntsiezina;
-        this.apurtuta = false;
-    }
-
-    public boolean suntsiezinaDa() {
-        return suntsiezina;
-    }
-
-    public boolean apurtutaDago() {
-        return apurtuta;
-    }
-
-    public  abstract void apurtu();
 }
