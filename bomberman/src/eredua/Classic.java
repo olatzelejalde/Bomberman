d8890a61c2b1aef42a101b2104af7b4ef9f5fc2d
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-03 20:42:39 +0100

    labo 3/3

--- bomberman/src/eredua/Classic.java
+++ bomberman/src/eredua/Classic.java
@@ -9,37 +9,41 @@ public class Classic extends Laberinto {
 		super();
 	}
 	
-	protected void sortuLaberinto() {
-		Random r = new Random();
-		
-		for (int i = 0; i < 11; i++) { //altuera
-			for (int j = 0; j < 17; j++) { //zabalera
-				// Bomberman posizioa hutsik hasieratu
-				if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
-					matriz[i][j] = new Gelaxka(null);
-				}
-				// Bloke gogorrak posizio bakoitietan
-				else if (i % 2 != 0 && j % 2 != 0) {
-					matriz[i][j] = new Gelaxka(new Blokea(false));
-				}
-				// Beste gelaxkak
-				else {
-					int prob1 = r.nextInt(100); // 0-100
-					// %40 baino handiago bloke biguna
-					if (prob1 > 40) {
-						matriz[i][j] = new Gelaxka(new Blokea(true));
-					}
-					else {
-						// %90 baino txikiagoa hutsik
-						int prob2 = r.nextInt(100); // 0-100
-						if (prob2 < 90) {
-							// Hurrengorako etsaia jartzeko
-							matriz[i][j] = new Gelaxka(null);
-						}
-					}
-				}
-			}
-		}
+	public void sortuLaberinto() {
+	    Random r = new Random();
+
+	    for (int i = 0; i < 11; i++) { // altura
+	        for (int j = 0; j < 17; j++) { // anchura
+	            // Bomberman inicia en una zona libre
+	            if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
+	                matriz[i][j] = new Gelaxka(null);
+	            }
+	            // Bloques duros en posiciones impares
+	            else if (i % 2 != 0 && j % 2 != 0) {
+	                matriz[i][j] = new Gelaxka(new Blokea(false));
+	            }
+	            // Otras posiciones: bloque blando o vacío
+	            else {
+	                int prob1 = r.nextInt(100); // 0-99
+	                if (prob1 < 40) { // 40% de probabilidad de bloque blando
+	                    matriz[i][j] = new Gelaxka(new Blokea(true));
+	                } else { // 60% de probabilidad de celda vacía
+	                    matriz[i][j] = new Gelaxka(null);
+	                }
+	            }
+	        }
+	    }
+	    for (int i = 0; i < 11; i++) {
+	        for (int j = 0; j < 17; j++) {
+	            char simbolo = (matriz[i][j].blokeDu() == false) ? ' ' : (matriz[i][j].apurtuDaiteke()== true? 'B' : 'G');
+	            System.out.print(simbolo + " ");
+	        }
+	        System.out.println();
+	    }
+
+	    setChanged();
+	    notifyObservers();
 	}
+
 
 }
