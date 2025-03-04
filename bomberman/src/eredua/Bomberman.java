bad96c5c2fab7d601087aa9352b9615a3907c471
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-04 19:31:23 +0100

    Update Bomberman.java

--- bomberman/src/eredua/Bomberman.java
+++ bomberman/src/eredua/Bomberman.java
@@ -2,11 +2,10 @@ package eredua;
 
 import java.util.Observable;
 
-public class Bomberman extends Observable{
+public abstract class Bomberman extends Observable{
 	private int x, y;
 	private boolean bizirik;
 	private Laberinto laberinto;
-	private int bonbaKop;
 	
 	public Bomberman(int x, int y, Laberinto laberinto) {
 		this.x = x;
@@ -30,14 +29,18 @@ public class Bomberman extends Observable{
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	public boolean hildaDago() {
+	    return !bizirik;
+	}
 
 	public void setHil(boolean hil) {
 		this.bizirik = false;
 	}
 	
-	public void mugitu(int newX, int newY, Laberinto laberinto) {
+	public void mugitu(int newX, int newY) {
 		// Laberintoaren limiteen barruan
-		if (laberinto != null && (newX >= 0 && newX < 11) && (newY >= 0 && newY < 17)) {
+		if (laberinto.koordenatuBarruan(newX, newY) && laberinto != null) {
 			// Posizio berrian bidea dagoen konprobatu
 			if (laberinto.bidePosizioa(newX, newY)) {
 				this.x = newX;
@@ -52,54 +55,10 @@ public class Bomberman extends Observable{
 		}
 		else {
 			System.out.println("Laberintotik kanpo");
-		}
-		
+		}	
 	}
 	
-	public void bonbaJarri(Laberinto laberinto) {
-		// Bonbak dituen konprobatu
-		if (bonbaKop > 0) { 
-			// Gelaxka hori hutsik badago begiratu
-			if (!laberinto.getGelaxkaPos(x,y).blokeDu()) {
-				laberinto.eguneratuGelaxka(x,y,null);
-				Bonba bonba = new Normal(x,y);
-				bonbaKop--;
-				System.out.println("Bonba (" + x + ", " + y + ") gelaxkan!!");
-
-
-			}
-			else {
-				System.out.println("Ezin da hemen bonba jarri husik ez dagoelako!!");
-			}	
-		}
-		// Ez dauka bonbarik, beraz 3s itxaron
-		else {
-			
-			System.out.println("Ez daukazu bonbarik! Beraz, itxaron behar duzu");
-			itxaronBonba();
-		}
-		setChanged();
-		notifyObservers();
-	}
-
-	public void itxaronBonba(){
-		if (bonbaKop == 0) {
-			new Thread(() -> {
-				try {
-					Thread.sleep(3000); // 3s itxaron bonba bat gehiago izateko
-					bonbaKop = 1;
-					System.out.println("Bonba bat gehiago duzu!!");
-				}
-				catch (InterruptedException e) {
-					e.printStackTrace();
-	        	}
-			}).start();
-		}
-	}
-	
-	public boolean hildaDago() {
-	    return !bizirik;
-	}
+	public abstract void bonbaJarri();
 	
 	public void hil() {
 	    if (bizirik) {
