ffc10dbd1ee35dd152eb5fbb7807ce9c2accc718
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-10 11:29:26 +0100

    Update Normal.java

--- bomberman/src/eredua/Normal.java
+++ bomberman/src/eredua/Normal.java
@@ -19,22 +19,24 @@ public class Normal extends Bonba {
 		Jokoa jokoa = Jokoa.getJokoa();
 		Laberinto laberinto = jokoa.getLaberinto();
 		
-		suntsituBlokea(x, y, laberinto); //posicion original de bomba
-		suntsituBlokea(x - 1, y, laberinto); //arriba
-		suntsituBlokea(x + 1, y, laberinto); //abajo
-		suntsituBlokea(x, y - 1, laberinto); //izquierda
-		suntsituBlokea(x, y + 1, laberinto); //derecha
+		// Marcar fuego en la posición de la bomba y sus alrededores si están vacíos
+		laberinto.jarriSua(x, y); //posicion original de bomba
+		laberinto.jarriSua(x + 1, y); //abajo
+		laberinto.jarriSua(x, y - 1); //izquierda
+		laberinto.jarriSua(x, y + 1); //derecha
 		
-		jokoa.kenduBonba(this);
-	}
-
-	public void suntsituBlokea(int x, int y, Laberinto laberinto) {
-		if (laberinto != null && laberinto.koordenatuBarruan(x,y)) {
-			Gelaxka g = laberinto.getGelaxkaPos(x, y);
-			
-			if (g != null && g.apurtuDaiteke()) {
-				laberinto.kenduBlokea(x,y);
-			}
-		}
+		// Esperar 2s y luego limpiar el fuego
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                laberinto.kenduSua(x, y);
+                laberinto.kenduSua(x - 1, y);
+                laberinto.kenduSua(x + 1, y);
+                laberinto.kenduSua(x, y - 1);
+                laberinto.kenduSua(x, y + 1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 	}
 }
