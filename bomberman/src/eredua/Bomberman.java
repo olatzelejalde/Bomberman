c55000ea8c1822de4752d2aa4358700ecce71176
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 11:59:06 +0100

    cambios importantes

--- bomberman/src/eredua/Bomberman.java
+++ bomberman/src/eredua/Bomberman.java
@@ -2,60 +2,69 @@ package eredua;
 
 import java.util.Observable;
 
-public abstract class Bomberman extends Observable{
+public abstract class Bomberman{
 	private int x, y;
 	private boolean bizirik;
-	private Laberinto laberinto;
+	protected int bonbaKop;
 	
-	public Bomberman(int x, int y, Laberinto laberinto) {
+	
+	public Bomberman(int x, int y, int bKop) {
 		this.x = x;
 		this.y = y;
-		this.laberinto = laberinto;
 		this.bizirik = true;
+		this.bonbaKop = bKop;
 	}
 
+	// Conseguir la posicion X
 	public int getX() {
 		return x;
 	}
 	
+	// Conseguir la posicion Y
 	public int getY() {
 		return y;
 	}
 	
+	// Colocarlo en la posicion x
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	// COlocarlo en la posicion y
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	// Verifica si ha muerto
 	public boolean hildaDago() {
 	    return !bizirik;
 	}
 
+	// Matar al bomberman
 	public void setHil(boolean hil) {
 		this.bizirik = false;
 	}
 	
+	// Metodo abstacto para poner bomba en Normal
+	public abstract void bonbaJarri();
+	
+	// Verifica si puede moverse a las nuevas coordenadas
 	public void mugitu(int newX, int newY) {
 		Laberinto laberinto = Jokoa.getJokoa().getLaberinto();
 		
-		// Laberintoaren limiteen barruan
+		// Mirar si esta dentro del laberinto
 		if (laberinto.koordenatuBarruan(newX, newY) && laberinto != null) {
-			// Posizio berrian bidea dagoen konprobatu
+			// Mirar si en la nueva posicion hay camino
 			if (laberinto.bidePosizioa(newX, newY)) {
-				this.x = newX;
-				this.y = newY;
-				setChanged();
-				notifyObservers();
-				System.out.println("Bomberman mugitu da: (" + x + ", " + y + ")");
+				// Quitar bomberman de la celda actual
+				laberinto.getGelaxkaPos(x, y).setBomberman(null);
+				// Mover a la celda nueva
+				laberinto.getGelaxkaPos(newX, newY).setBomberman(this);
 				
-				// Si hay fuego en la nueva casilla muere
-				if (laberinto.getGelaxkaPos(newX, newY).suaDago()) {
-					hil();
-					Jokoa.getJokoa().bukaera(true);
-				}
+				setX(newX);
+				setY(newY);
+						
+				System.out.println("Bomberman mugitu da: (" + x + ", " + y + ")");
 			}
 			else {
 				System.out.println("Ezin da mugitu posizio honetara");
@@ -66,14 +75,11 @@ public abstract class Bomberman extends Observable{
 		}	
 	}
 	
-	public abstract void bonbaJarri();
-	
+	// Matar al bomberman y acabar partida
 	public void hil() {
 	    if (bizirik) {
 	        bizirik = false;
 	        Jokoa.getJokoa().bukaera(false);
 	    }
-	    setChanged();
-        notifyObservers();	
 	}
 }
