b7e68a151fd0319881ba69ba77078e251197d387
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 12:06:04 +0100

    cambios importantes

--- bomberman/src/eredua/Normal.java
+++ bomberman/src/eredua/Normal.java
@@ -2,28 +2,30 @@ package eredua;
 
 public class Normal extends Bonba {
 	
+	
 	public Normal(int x, int y) {
 		super(x,y);
 	}
 	
+	// Metodo para la explosion de la bomba
 	@Override
 	public void eztanda() {
-		if (!aktibatuta) {
-			return;
-		}
 		aktibatuta = false;
 		
 		int x = getX();
 		int y = getY();
 		
-		Jokoa jokoa = Jokoa.getJokoa();
-		Laberinto laberinto = jokoa.getLaberinto();
+		Laberinto laberinto = Jokoa.getJokoa().getLaberinto();
 		
 		// Marcar fuego en la posición de la bomba y sus alrededores si están vacíos
 		laberinto.jarriSua(x, y); //posicion original de bomba
+		laberinto.jarriSua(x - 1, y); //arriba
 		laberinto.jarriSua(x + 1, y); //abajo
 		laberinto.jarriSua(x, y - 1); //izquierda
 		laberinto.jarriSua(x, y + 1); //derecha
+		
+		// Eliminar la bomba de la celda
+		laberinto.getMatriz()[x][y].setBonba(null);
 		
 		// Esperar 2s y luego limpiar el fuego
         new Thread(() -> {
