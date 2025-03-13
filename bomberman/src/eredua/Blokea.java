23ff80c81a1a93357bbbf13025b29b823c5cf0db
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 11:58:43 +0100

    cambios importantes

--- bomberman/src/eredua/Blokea.java
+++ bomberman/src/eredua/Blokea.java
@@ -3,16 +3,14 @@ package eredua;
 public class Blokea {
 	protected boolean suntsigarri;
 	
+	
 	public Blokea(boolean suntsigarri) {
 		this.suntsigarri = suntsigarri;
 	}
 	
+	// Verificar si es rompible
 	public boolean suntsigarriaDa() {
 		return suntsigarri;
-	}
-	
-	protected void apurtu() {
-		//TODO
 	}
 
 }
