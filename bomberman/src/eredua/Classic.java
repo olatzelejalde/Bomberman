9dde6cdb29509187a552b163aa72adf809dd97c2
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 12:04:14 +0100

    cambios importantes

--- bomberman/src/eredua/Classic.java
+++ bomberman/src/eredua/Classic.java
@@ -1,14 +1,15 @@
-
 package eredua;
 
 import java.util.Random;
 
 public class Classic extends Laberinto {
 	
+	
 	public Classic() {
 		super();
 	}
 	
+	// Metodo para crear el laberinto
 	public void sortuLaberinto() {
 	    Random r = new Random();
 
@@ -16,19 +17,22 @@ public class Classic extends Laberinto {
 	        for (int j = 0; j < 17; j++) { // anchura
 	            // Bomberman inicia en una zona libre
 	            if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
-	                matriz[i][j] = new Gelaxka(null);
+	                matriz[i][j] = new Gelaxka(null, null);
+	                if (i == 0 && j == 0) {
+	                	matriz[i][j] = new Gelaxka(null, Jokoa.getJokoa().getBomberman());
+	                }
 	            }
 	            // Bloques duros en posiciones impares
 	            else if (i % 2 != 0 && j % 2 != 0) {
-	                matriz[i][j] = new Gelaxka(new Blokea(false));
+	                matriz[i][j] = new Gelaxka(new Blokea(false), null);
 	            }
 	            // Otras posiciones: bloque blando o vacío
 	            else {
 	                int prob1 = r.nextInt(100); // 0-99
 	                if (prob1 < 40) { // 40% de probabilidad de bloque blando
-	                    matriz[i][j] = new Gelaxka(new Blokea(true));
+	                    matriz[i][j] = new Gelaxka(new Blokea(true), null);
 	                } else { // 60% de probabilidad de celda vacía
-	                    matriz[i][j] = new Gelaxka(null);
+	                    matriz[i][j] = new Gelaxka(null, null);
 	                }
 	            }
 	        }
