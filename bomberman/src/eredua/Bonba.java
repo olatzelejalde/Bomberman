1a37171b257dd85189adddea8c23947375a823e4
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 12:01:24 +0100

    cambios importantes

--- bomberman/src/eredua/Bonba.java
+++ bomberman/src/eredua/Bonba.java
@@ -4,23 +4,28 @@ public abstract class Bonba {
 	private int x, y;
 	protected boolean aktibatuta;
 
+	
 	public Bonba(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.aktibatuta = true;
 	}
 	
+	// Conseguir la psoicion X
 	public int getX() {
 		return x;
 	}
 	
+	// Conseguir la posicion Y
 	public int getY() {
 		return y;
 	}
 	
+	// Verificar si la bomba esta activada
 	public boolean aktibatutaDago() {
 		return aktibatuta;
 	}
 	
+	// Metodo abstracto para explotar en Normal
 	protected abstract void eztanda();
 }
