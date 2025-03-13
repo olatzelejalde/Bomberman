4856bd3cd853c1c3e4a348f804478e64b2ced0bf
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-13 12:04:48 +0100

    cambios importantes

--- bomberman/src/eredua/Gelaxka.java
+++ bomberman/src/eredua/Gelaxka.java
@@ -1,37 +1,86 @@
 package eredua;
 
-public class Gelaxka {
+import java.util.Observable;
+
+public class Gelaxka extends Observable {
 	private Blokea bloke;
 	private Bonba bonba;
 	private boolean sua;
+	private Bomberman bomberman;
 
-	public Gelaxka(Blokea bloke) {
+	
+	public Gelaxka(Blokea bloke, Bomberman bomberman) {
 		this.bloke = bloke;
 		this.bonba = null;
 		this.sua = false;
+		this.bomberman = bomberman;
 	}
 
+	// Verificar si hay bloque en la celda
 	public boolean blokeDu() {
 		return bloke != null;
 	}
 	
+	// Verificar si se puede romper el bloque
 	public boolean apurtuDaiteke() {
-		return bloke.suntsigarriaDa();
+		return bloke != null && bloke.suntsigarriaDa();
 	}
 	
+	// Romper el bloque si es rompible
 	public void apurtuBlokea() {
 		if (apurtuDaiteke()) {
 			bloke = null;
 			System.out.println("Blokea apurtu da!!");
+			eguneratuBista();
 		}
 	}
 	
+	// Verificar si hay fuego en la celda
 	public boolean suaDago() {
 		return sua != false;
 	}
-
+	
+	// Verificar si esta el bomberman en la celda
+	public boolean bombermanDago() {
+		if (bomberman != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// Verificar si hay bomba en la celda
+	public boolean bonbaDago() {
+		if (bonba != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// Poner bomberman en la celda
+	public void setBomberman(Bomberman b) {
+		bomberman = b;
+		eguneratuBista();
+	}
+	
+	// Poner bomba en la celda
+	public void setBonba(Bonba bonba) {
+		this.bonba = bonba;
+		eguneratuBista();
+	}
+	
+	// Poner fuego en la celda
 	public void setSua(boolean sua) {
 		this.sua = sua;
+		eguneratuBista();
 	}
-
+	
+	// Notificar a la vista de cambios
+	public void eguneratuBista() {
+		setChanged();
+        notifyObservers();
+	}
 }
