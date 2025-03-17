f797bd47c2480a3bca07bdd39f55e6cccae2cf40
Ainoaa <81186158+Ainoaa@users.noreply.github.com>
2025-03-17 18:34:11 +0100

    Update Classic.java

--- bomberman/src/eredua/Classic.java
+++ bomberman/src/eredua/Classic.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class Classic extends Laberinto {
 	
-	// Eraikitzailea
 	public Classic() {
 		super();
 	}
@@ -15,30 +14,35 @@ public class Classic extends Laberinto {
 
 	    for (int i = 0; i < 11; i++) { // altuera
 	        for (int j = 0; j < 17; j++) { // zabalera
-	            // Bombermana zona libre batean hasiko da
+	        	// Bombermana gelaxka libre batean hasiko da
 	            if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
-	                matriz[i][j] = new Gelaxka(null, null);
 	                if (i == 0 && j == 0) {
-	                	matriz[i][j] = new Gelaxka(null, Jokoa.getJokoa().getBomberman());
+	                	matriz[i][j] = new Gelaxka(null, true);
+	                }
+	                else {
+		                matriz[i][j] = new Gelaxka(null, false);
 	                }
 	            }
+	            
 	            // Bloke gogorrak posizio bakoitietan
 	            else if (i % 2 != 0 && j % 2 != 0) {
-	                matriz[i][j] = new Gelaxka(new Blokea(false), null);
+	                matriz[i][j] = new Gelaxka(new Blokea(false), false);
 	            }
 	            // Beste posizioak: bloke bigunak edo hutsik
 	            else {
 	                int prob1 = r.nextInt(100); // 0-99
-	                if (prob1 < 40) { // Bloke bigunen probabilitatea 40% 
-	                    matriz[i][j] = new Gelaxka(new Blokea(true), null);
+	                if (prob1 < 40) { 
+	                	// Bloke bigunen probabilitatea 40% 
+	                    matriz[i][j] = new Gelaxka(new Blokea(true), false);
 	                    Laberinto.getLaberinto().gehituSuntsigarri();
-	                } else { // Gelaxka hutsaren probabilitatea 60%
-	                    matriz[i][j] = new Gelaxka(null, null);
+	                } else { 
+	                	// Gelaxka hutsaren probabilitatea 60%
+	                    matriz[i][j] = new Gelaxka(null, false);
 	                }
 	            }
 	        }
 	    }
+	    setChanged();
+	    notifyObservers();
 	}
-
-
 }
