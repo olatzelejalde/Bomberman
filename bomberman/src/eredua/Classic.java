669cc87c1d8db76bda84e0699d04691500127943
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2025-03-17 17:54:09 +0100

    Update Classic.java

--- bomberman/src/eredua/Classic.java
+++ bomberman/src/eredua/Classic.java
@@ -4,35 +4,35 @@ import java.util.Random;
 
 public class Classic extends Laberinto {
 	
-	
+	// Eraikitzailea
 	public Classic() {
 		super();
 	}
 	
-	// Metodo para crear el laberinto
+	// Labirintoa sortzeko metodoa
 	public void sortuLaberinto() {
 	    Random r = new Random();
 
-	    for (int i = 0; i < 11; i++) { // altura
-	        for (int j = 0; j < 17; j++) { // anchura
-	            // Bomberman inicia en una zona libre
+	    for (int i = 0; i < 11; i++) { // altuera
+	        for (int j = 0; j < 17; j++) { // zabalera
+	            // Bombermana zona libre batean hasiko da
 	            if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
 	                matriz[i][j] = new Gelaxka(null, null);
 	                if (i == 0 && j == 0) {
 	                	matriz[i][j] = new Gelaxka(null, Jokoa.getJokoa().getBomberman());
 	                }
 	            }
-	            // Bloques duros en posiciones impares
+	            // Bloke gogorrak posizio bakoitietan
 	            else if (i % 2 != 0 && j % 2 != 0) {
 	                matriz[i][j] = new Gelaxka(new Blokea(false), null);
 	            }
-	            // Otras posiciones: bloque blando o vacío
+	            // Beste posizioak: bloke bigunak edo hutsik
 	            else {
 	                int prob1 = r.nextInt(100); // 0-99
-	                if (prob1 < 40) { // 40% de probabilidad de bloque blando
+	                if (prob1 < 40) { // Bloke bigunen probabilitatea 40% 
 	                    matriz[i][j] = new Gelaxka(new Blokea(true), null);
 	                    Laberinto.getLaberinto().gehituSuntsigarri();
-	                } else { // 60% de probabilidad de celda vacía
+	                } else { // Gelaxka hutsaren probabilitatea 60%
 	                    matriz[i][j] = new Gelaxka(null, null);
 	                }
 	            }
