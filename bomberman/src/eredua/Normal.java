5298c7a302880f589c5053647e09a97815f930f1
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2025-03-20 11:03:13 +0100

    Update Normal.java

--- bomberman/src/eredua/Normal.java
+++ bomberman/src/eredua/Normal.java
@@ -1,81 +1,107 @@
 package eredua;
 
-import java.util.Timer;
-import java.util.TimerTask;
+public abstract class Laberinto {
+	private static Laberinto nireLaberinto;
+	protected Gelaxka[][] matriz;
+	private int suntsigarriak;
 
-public class Normal extends Bonba {
-	private Timer suaTimer, eztandaTimer, tocarFuegoTimer;
 	
-	
-	public Normal(int x, int y) {
-		super(x,y);
+	protected Laberinto() {
+		this.matriz = new Gelaxka[11][17];
+		this.suntsigarriak = 0;
+	}
+
+	public static Laberinto getLaberinto() {
+		if (nireLaberinto == null) {
+			nireLaberinto = new Classic();
+		}
+		return nireLaberinto;
 	}
 	
-	// Leherketarako timerra hasieratu
-    @Override
-    public void hasiEztanda() {
-    	// 3 segundo itxaron eta sua jarri
-        eztandaTimer = new Timer();
-        eztandaTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                eztanda(); 
-            }     
-        }, 3000);
-    }
+	// Metodo abstraktua labirintoa sortzeko
+	public abstract void sortuLaberinto();
 	
-	// Bonbaren eztanda kontrolatzeko metodoa
-	public void eztanda() {
-		aktibatuta = false;
-		
-		int x = getX();
-		int y = getY();
-		
-		Laberinto laberinto = Jokoa.getJokoa().getLaberinto();
-		Gelaxka g = laberinto.getMatriz()[x][y];
-		
-		// Sua jarri bonbaren posizioan eta ondokoetan
-		laberinto.jarriSua(x, y); // Bonbaren posizioa
-		laberinto.jarriSua(x - 1, y); // Goiko gelaxka
-		laberinto.jarriSua(x + 1, y); // Beheko gelaxka
-		laberinto.jarriSua(x, y - 1); // Ezkerreko gelaxka
-		laberinto.jarriSua(x, y + 1); // Eskumako gelaxka
-		
-		// Gelaxkatik bonba kendu
-		g.setBonba(null);
-		
-		// 500ms-ro egiaztatu bomberman-a ez duela sua ukitu
-		tocarFuegoTimer = new Timer();
-		tocarFuegoTimer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-            public void run() {
-                // Bomberman-aren momentuko posizioa gorde  
-                int bombermanX = Jokoa.getJokoa().getBomberman().getX();
-                int bombermanY = Jokoa.getJokoa().getBomberman().getY();
+	// Matrizea lortu
+	public Gelaxka[][] getMatriz() {
+		return matriz;
+	}
+	
+	// Gelaxkaren posizioa lortu
+	public Gelaxka getGelaxkaPos(int x, int y) {
+		if (koordenatuBarruan(x,y)) {
+			return matriz[x][y];
+		}
+		return null;
+	}
+	
+	// Koordenatuen barnean dagoen egiaztatu
+	public boolean koordenatuBarruan(int x, int y) {
+		return (x >= 0 && x < 11) && (y >= 0 && y < 17);
+	}
 
-                // Egiaztatu bomberman ea sua daukan gelaxka batean dagoen
-                if (laberinto.getMatriz()[bombermanX][bombermanY].suaDago()) {
-                    Jokoa.getJokoa().getBomberman().hil();
-                    Jokoa.getJokoa().bukaera(false);
-                    // Egiaztapena eten
-                    this.cancel();
-                }
-            }
-        }, 0, 500); 
+	// Bidea dagoen egiaztatu
+	public boolean bidePosizioa(int x, int y) {
+		return !matriz[x][y].blokeDu() && !matriz[x][y].bonbaDago();
+	}
+
+	// Gelaxka eguneratu egoera aldatu bada
+	public void eguneratuGelaxka(int x, int y, Blokea bloke) {
+		if (koordenatuBarruan(x,y)) {
+			matriz[x][y] = new Gelaxka(bloke, false);
+		}
+	}
+	
+	// Bloke bigunak dauden egiaztatu
+	public boolean blokeakDaude() {
+		return this.suntsigarriak > 0;
+	}
+	
+	// Bloke suntsigarria gehitu
+	public void gehituSuntsigarri() {
+		this.suntsigarriak++;
+	}
+
+	// Bloke suntsigarria kendu
+	public void kenduSuntsigarri() {
+		if (suntsigarriak > 0) {
+			this.suntsigarriak--;
+		}
 		
-		// 2 segundo itxaron
-        suaTimer = new Timer();
-        suaTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-            	// Sua kendu
-                laberinto.kenduSua(x, y);
-                laberinto.kenduSua(x - 1, y);
-                laberinto.kenduSua(x + 1, y);
-                laberinto.kenduSua(x, y - 1);
-                laberinto.kenduSua(x, y + 1);
-                tocarFuegoTimer.cancel();
-            }
-        }, 2000); 
-	}	
+		if (suntsigarriak == 0) {
+			Jokoa.getJokoa().bukaera(true);
+		}
+	}
+	
+	// Alboko gelaxketan sua jarri eta blokea biguna bada, apurtu
+	public void jarriSua(int x, int y) {
+		if (koordenatuBarruan(x,y)) {
+			Gelaxka g = matriz[x][y];
+			
+			// Bakarrik sua jarriko du gelaxka hutsik badago edo bloke biguna bada
+			if (!g.blokeDu() || g.apurtuDaiteke() || g.bombermanDago()) {
+				g.setSua(true);
+				
+				// Bloke biguna bada, apurtu
+				if (g.apurtuDaiteke()) {
+					g.apurtuBlokea();
+				}
+				// Bomberman-a hil baldin eta dagoen gelaxkan sua dago
+				if (g.bombermanDago()) {
+					Jokoa.getJokoa().getBomberman().hil();
+					g.setBomberman(false);
+					Jokoa.getJokoa().bukaera(false);
+				}
+			}
+		}
+	}
+	
+	// Sua kendu eztanda denbora amaitu denean
+	public void kenduSua(int x, int y) {
+		if (koordenatuBarruan(x,y)) {
+			Gelaxka g = matriz[x][y];
+			if (g.suaDago()) {
+				g.setSua(false);
+			}
+		}
+	}
 }
