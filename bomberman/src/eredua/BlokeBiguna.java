db6de8ef906eefa6373b7fc8dc1780f2156cbca3
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2025-03-27 11:06:12 +0100

    Update BlokeBiguna.java

--- bomberman/src/eredua/BlokeBiguna.java
+++ bomberman/src/eredua/BlokeBiguna.java
@@ -1,13 +1,13 @@
 package eredua;
 
-//Bloke Gogorra: Ez suntsigarria den bloke bat
-public class BlokeGogorra extends Blokea {
-	public BlokeGogorra() {
-		super(false); // Bloke gogorrak ez dira suntsigarriak
+//Bloke Biguna: Suntsigarria den bloke bat
+public class BlokeBiguna extends Blokea {
+	public BlokeBiguna() {
+		super(true); // Bloke bigunak suntsigarriak dira
 	}
 	
 	@Override
     public boolean blokeBigunaDa() {
-        return false;
+        return true;
     }
 }
