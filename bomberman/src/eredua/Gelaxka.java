55ee7985667374f67d92a2540c4729a24d83a06c
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-30 20:52:41 +0200

    Update Gelaxka.java

--- bomberman/src/eredua/Gelaxka.java
+++ bomberman/src/eredua/Gelaxka.java
@@ -5,13 +5,14 @@ import java.util.Observable;
 public class Gelaxka extends Observable {
 	private Blokea bloke;
 	private Bonba bonba;
+	private boolean etsai;
 	private boolean sua;
 	private boolean bomberman;
 
-	// Eraikitzaileak
 	public Gelaxka(Blokea bloke, boolean bomberman) {
 		this.bloke = bloke;
 		this.bonba = null;
+		this.etsai = etsai;
 		this.sua = false;
 		this.bomberman = bomberman;
 	}
@@ -26,6 +27,11 @@ public class Gelaxka extends Observable {
 		return bloke != null && bloke.blokeBigunaDa();
 	}
 	
+	// Etsaia pasatzeko bidea dagoen jakiteko
+	public boolean pasatuDaiteke() {
+	    return !blokeDu() && !bonbaDago() && !etsaiaDago() && !bombermanDago();
+	}
+	
 	// Blokea apurtu apurtu ahal bada
 	public void apurtuBlokea() {
 		if (apurtuDaiteke()) {
@@ -36,24 +42,24 @@ public class Gelaxka extends Observable {
 		}
 	}
 	
+	// Gelaxkan etsaia dagoen jakiteko
+	public boolean etsaiaDago() {
+		return etsai;
+	}
+	
 	// Gelaxkan sua dagoen jakiteko
 	public boolean suaDago() {
 		return sua;
 	}
 	
-	// Gelaxkan Bombermana dagoen jakiteko
+	// Gelaxkan Bomberman dagoen jakiteko
 	public boolean bombermanDago() {
 		return bomberman;
 	}
 	
 	// Gelaxkan bonba dagoen jakiteko
 	public boolean bonbaDago() {
-		if (bonba != null) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return bonba != null;
 	}
 	
 	// Bombermana gelaxkan jarri
@@ -74,24 +80,40 @@ public class Gelaxka extends Observable {
 		eguneratuBista();
 	}
 	
+	// Sua gelaxkan jarri
+	public void setEtsaia(boolean etsai) {
+		this.etsai = etsai;
+		eguneratuBista();
+	}
+	
 	// Aldaketetaz jakinarazi bistari
 	public void eguneratuBista() {
         String egoera;
+        
         if (this.sua) {
             egoera = "sua";
-        } else if (bomberman && bonbaDago()) {
+        } 
+        else if (bomberman && bonbaDago()) {
         	egoera = "bombermanBonba";
-        } else if (bomberman) {
+        } 
+        else if (bomberman) {
         	egoera = "bomberman";
-        } else if (bonbaDago()) {
+        } 
+        else if (bonbaDago()) {
         	egoera = "bonba";
-        } else if (blokeDu()) {
+        } 
+        else if (etsaiaDago()) {
+        	egoera = "bonba";
+        } 
+        else if (blokeDu()) {
             if (apurtuDaiteke()) {
             	egoera = "blokBig";
-            } else {
+            } 
+            else {
             	egoera = "blokGo";
             }
-        } else {
+        } 
+        else {
         	egoera = "hutsik";
         }
         setChanged();
