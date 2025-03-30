3e8d0819cfad7aa49a552a8361862d5d35f7db28
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-30 20:53:45 +0200

    Create Soft.java

---
+++ bomberman/src/eredua/Soft.java
@@ -0,0 +1,56 @@
+package eredua;
+
+import java.util.Random;
+
+public class Soft extends Laberinto {
+    private int etsaiKop;
+    
+    public Soft() {
+        super();
+        etsaiKop = 0;
+    }
+    
+    public void sortuLaberinto() {
+        Random r = new Random();
+        Gelaxka[][] matriz = super.getMatriz();
+
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 17; j++) {
+                // Bomberman gelaxka libre batean hasiko da
+                if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
+                    if (i == 0 && j == 0) {
+                        matriz[i][j] = new Gelaxka(null, true);
+                    }
+                    else {
+                        matriz[i][j] = new Gelaxka(null, false);
+                    }
+                }
+                
+                // Bloke gogorrak posizio bakoitietan
+                else if (i % 2 != 0 && j % 2 != 0) {
+                    matriz[i][j] = new Gelaxka(new BlokeGogorra(), false);
+                }
+                
+                // Beste posizioak
+                else {
+                    int prob1 = r.nextInt(100);
+                    if (prob1 < 40) {
+                        // Bloke biguna (40% probabilitatea)
+                        matriz[i][j] = new Gelaxka(new BlokeBiguna(), false);
+                        Laberinto.getLaberinto().gehituSuntsigarri();
+                    } 
+                    else {
+                        // Bigarren probabilitatea etsaientzat
+                        int prob2 = r.nextInt(100);
+                        if (prob2 > 0 && etsaiKop < 8) {
+                            matriz[i][j] = new Gelaxka(new Etsaia(), false);
+                            etsaiKop++;
+                        } else {
+                            matriz[i][j] = new Gelaxka(null, false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
