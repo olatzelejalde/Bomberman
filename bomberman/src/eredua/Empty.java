bb817e6ef70a66d5436162ee2d385bc96f0db217
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-03-31 20:49:13 +0200

    Update Empty.java

--- bomberman/src/eredua/Empty.java
+++ bomberman/src/eredua/Empty.java
@@ -13,6 +13,7 @@ public class Empty extends Laberinto {
     public void sortuLaberinto() {
         Random r = new Random();
         Gelaxka[][] matriz = super.getMatriz();
+        BlokeFactory blokeFactory = BlokeFactory.getBlokeFactory();
 
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 17; j++) {
@@ -25,6 +26,13 @@ public class Empty extends Laberinto {
                         matriz[i][j] = new Gelaxka(null, false);
                     }
                 }
+                
+                // Bloke gogorrak posizio bakoitietan
+                else if (i % 2 != 0 && j % 2 != 0) {
+                	Blokea blokeGog = blokeFactory.createBloke(BlokeFactory.GOGORRA);
+                    matriz[i][j] = new Gelaxka(blokeGog, false);
+                }
+                
                 // Beste posizioak: etsaiak edo hutsik
                 else {
                     int prob = r.nextInt(100);
