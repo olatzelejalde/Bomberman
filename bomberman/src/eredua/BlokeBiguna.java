3f0c538cee963de94a5d95bf93f140632d73e64a
Ainoaa <81186158+Ainoaa@users.noreply.github.com>
2025-03-31 17:24:38 +0200

    Update BlokeBiguna.java
    iups

--- bomberman/src/eredua/BlokeBiguna.java
+++ bomberman/src/eredua/BlokeBiguna.java
@@ -5,7 +5,8 @@ public class BlokeBiguna extends Blokea {
 	public BlokeBiguna() {
 		super(true); // Bloke bigunak suntsigarriak dira
 	}
-	
+
+	@Override
     public boolean blokeBigunaDa() {
         return true;
     }
