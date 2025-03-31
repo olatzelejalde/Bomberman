d9ed95542b655b4e70c9a595ef59ef744dc5d296
MaiMai <70096367+maidertato@users.noreply.github.com>
2025-03-31 17:48:37 +0200

    Update Black.java

--- bomberman/src/eredua/Black.java
+++ bomberman/src/eredua/Black.java
@@ -7,10 +7,50 @@ public class Black extends Bomberman {
 	private boolean bonbaItxaroten;
 	private Timer bonbaTimer;
 	
+	// Eraikitzailea
 	public Black(int x, int y, int bonbaKop) {
 		super(x, y, bonbaKop);
 		this.bonbaItxaroten = false;		
 	}
 	
+	// Bomberman-a dituen bonba bakarra dauka : ultrabomb.
+	// Bomberman-a dituen bonba kopurua lortu
+	public int getBonbaKop() {
+		return super.getBonbaKop();
+	}
 	
+	// Bonba bat 3 segunduro jarri ahalko ditu 
+	@Override
+	public void bonbaJarri() {
+		if (getBonbaKop() == 1) {
+			Jokoa.getJokoa().kokatuBonba();
+			super.setBonbaKop(0);
+			System.out.println("Bomba kokatu duzu. Bomba berria jartzeko 3 segundo itxaron.");	
+		}
+		// Bonbarik ez badauka, 3 segundo itxaron beharko ditu beste bonba bat lortzeko 
+		else if (!bonbaItxaroten) {
+			System.out.println("Ez daukazu bonbarik!! 3 segundo itxaron bonba bat lortzeko.");
+			itxaronBonba();
+		}
+		// Bonba jada kokatzen ari da
+		else {
+			System.out.println("Bonba bat lortzen ari zara, itxaron!!");
+		}
+	}
+
+	// Metodo honek bonba bat lortzeko itxaron beharreko denbora kontrolatuko du
+	public void itxaronBonba() {
+		bonbaItxaroten = true;
+			
+		// 3 segundo itxaron eta bonba bat lortu
+		bonbaTimer = new Timer();
+	    bonbaTimer.schedule(new TimerTask() {
+	    	@Override
+	        public void run() {
+	          	setBonbaKop(1);
+	            bonbaItxaroten = false; 
+	            System.out.println("Bonba bat gehiago duzu!!");
+	        }
+	    }, 3000); 
+	}
 }
