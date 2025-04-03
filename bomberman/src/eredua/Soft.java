b6ff5a616b939494610d1f0f6682f8f1643918bc
Ainoaa <81186158+Ainoaa@users.noreply.github.com>
2025-04-03 17:52:01 +0200

    Update Soft.java

--- bomberman/src/eredua/Soft.java
+++ bomberman/src/eredua/Soft.java
@@ -3,11 +3,9 @@ package eredua;
 import java.util.Random;
 
 public class Soft extends Laberinto {
-    private int etsaiKop;
     
     public Soft() {
         super();
-        etsaiKop = 0;
     }
     
     public void sortuLaberinto() {
@@ -20,10 +18,10 @@ public class Soft extends Laberinto {
                 // Bomberman gelaxka libre batean hasiko da
                 if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
                     if (i == 0 && j == 0) {
-                        matriz[i][j] = new Gelaxka(null, true);
+                        matriz[i][j] = new Gelaxka(null, true, false);
                     }
                     else {
-                        matriz[i][j] = new Gelaxka(null, false);
+                        matriz[i][j] = new Gelaxka(null, false, false);
                     }
                 }
                 
@@ -33,17 +31,19 @@ public class Soft extends Laberinto {
                     if (prob1 < 40) {
                         // Bloke biguna (40% probabilitatea)
                     	Blokea blokeBiguna = blokeFactory.createBloke(BlokeFactory.BIGUNA);
-                        matriz[i][j] = new Gelaxka(blokeBiguna, false);
+                        matriz[i][j] = new Gelaxka(blokeBiguna, false, false);
                         gehituSuntsigarri();
                     } 
                     else {
                         // Bigarren probabilitatea etsaientzat
                         int prob2 = r.nextInt(100);
-                        if (prob2 > 0 && etsaiKop < 8) {
-                            matriz[i][j] = new Gelaxka(new Etsaia(), false);
-                            etsaiKop++;
+                        if (prob2 > 0 && getEtsaiak() < 8) {
+                        	// Birpasatu Gelaxka eraikitzailea
+                        	// matriz[i][j] = new Gelaxka(null, false, true);
+                            matriz[i][j] = new Gelaxka(new Etsaia(), false, true);
+                            gehituEtsaia();
                         } else {
-                            matriz[i][j] = new Gelaxka(null, false);
+                            matriz[i][j] = new Gelaxka(null, false, false);
                         }
                     }
                 }
