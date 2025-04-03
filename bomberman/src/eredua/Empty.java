20a96356ef374f3a3872bf7b7fba84b41506fb7c
Ainoaa <81186158+Ainoaa@users.noreply.github.com>
2025-04-03 18:08:34 +0200

    Update Empty.java

--- bomberman/src/eredua/Empty.java
+++ bomberman/src/eredua/Empty.java
@@ -3,11 +3,9 @@ package eredua;
 import java.util.Random;
 
 public class Empty extends Laberinto {
-	private int etsaiKop;
     
     public Empty() {
         super();
-        etsaiKop = 0;
     }
     
     public void sortuLaberinto() {
@@ -15,34 +13,15 @@ public class Empty extends Laberinto {
         Gelaxka[][] matriz = super.getMatriz();
         BlokeFactory blokeFactory = BlokeFactory.getBlokeFactory();
 
+        // Bomberman (0,0) gelaxkan jarri
+        matriz[0][0] = new Gelaxka(null, true, false);
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 17; j++) {
-                // Bomberman gelaxka libre batean hasiko da
-                if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
-                    if (i == 0 && j == 0) {
-                        matriz[i][j] = new Gelaxka(null, true);
-                    }
-                    else {
-                        matriz[i][j] = new Gelaxka(null, false);
-                    }
-                }
-                
-                // Bloke gogorrak posizio bakoitietan
-                else if (i % 2 != 0 && j % 2 != 0) {
-                	Blokea blokeGog = blokeFactory.createBloke(BlokeFactory.GOGORRA);
-                    matriz[i][j] = new Gelaxka(blokeGog, false);
-                }
-                
-                // Beste posizioak: etsaiak edo hutsik
-                else {
-                    int prob = r.nextInt(100);
-                    if (prob > 95 && etsaiKop < 10) {
-                        matriz[i][j] = new Gelaxka(new Etsaia(), false);
-                        etsaiKop++;
-                    } 
-                    else {
-                        matriz[i][j] = new Gelaxka(null, false);
-                    }
+                if (Math.random() > 0.95 && getEtsaiak() < 10) {
+                	// Birpasatu Gelaxka eraikitzailea
+                	// matriz[i][j] = new Gelaxka(null, false, true);
+                	matriz[i][j] = new Gelaxka(new Etsaia(), false, true);
+                	gehituEtsaia();
                 }
             }
         }
