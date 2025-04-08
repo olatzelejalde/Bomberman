a2679a85a69207e03f08402d97c59a89f2ae8eca
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-04-08 21:58:39 +0200

    Update BombermanFactory.java

--- bomberman/src/eredua/BombermanFactory.java
+++ bomberman/src/eredua/BombermanFactory.java
@@ -2,29 +2,36 @@ package eredua;
 
 public class BombermanFactory {
 	private static BombermanFactory nBF;
-	
-	private BombermanFactory() {
-		
-	}
-
-	public static BombermanFactory getBF() {
-		if (nBF == null) {
-			nBF = new BombermanFactory();
-		}
-		return nBF;
-	}
-	
-	public Bomberman sortuBomberman(String pMota) {
-		switch(pMota) {
-		case "White":
-			return new White(0, 0, 10);
-		
-		case "Black":
-			return new Black(0, 0, 0);
-		
-		default:
-			return new White(0, 0, 10);
-		}
-			
-	}
+	private String white, black;
+    
+   
+    private BombermanFactory() {
+    	
+    }
+    
+    public static BombermanFactory getBF() {
+        if (nBF == null) {
+        	nBF = new BombermanFactory();
+        }
+        return nBF;
+    }
+    
+    public Bomberman createBomberman(String mota) {
+        if (mota == null) {
+            throw new IllegalArgumentException("Mota ezin daiteke null izan");
+        }
+        
+        Bomberman b = null;
+        
+        switch(mota.toLowerCase()) {
+            case "white":
+                b = new White(0,0);
+                break;
+            case "black":
+            	b = new Black(0,0);
+                break;
+        }
+        return b;
+    }
 }
+
