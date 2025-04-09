a6927e5e049fed0199d7ee3af511821bcf716f38
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-04-09 11:11:41 +0200

    Update Nagusia.java

--- bomberman/src/Nagusia.java
+++ bomberman/src/Nagusia.java
@@ -1,13 +1,17 @@
 package main;
 
 import javax.swing.SwingUtilities;
-
+import bista.Menu;
 import bista.Partida;
 import eredua.Jokoa;
 
+
 public class Nagusia {
+	
 	public static void main(String[] args) {
-         new Partida();
-         Jokoa.getJokoa().hasiJokoa();
+        SwingUtilities.invokeLater(() -> {
+        	Menu menu = new Menu();
+        	menu.setVisible(true);            
+        });
     }
 }
