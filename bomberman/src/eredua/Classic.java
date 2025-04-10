c168f4dbb82fbfb888681359af3bb9fda3988c0e
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-04-10 13:10:29 +0200

    Update Classic.java

--- bomberman/src/eredua/Classic.java
+++ bomberman/src/eredua/Classic.java
@@ -14,7 +14,8 @@ public class Classic extends Laberinto {
 	    Random r = new Random();
 	    Gelaxka[][] matriz = super.getMatriz();
 	    BlokeFactory blokeFactory = BlokeFactory.getBlokeFactory();
-
+	    Jokoa jokoa = Jokoa.getJokoa();
+	    
 	    for (int i = 0; i < 11; i++) { // altuera
 	        for (int j = 0; j < 17; j++) { // zabalera
 	        	// Bomberman gelaxka libre batean hasiko da
@@ -29,7 +30,7 @@ public class Classic extends Laberinto {
 	            
 	            // Bloke gogorrak posizio bakoitietan
 	            else if (i % 2 != 0 && j % 2 != 0) {
-	            	Bloke blokeGog = blokeFactory.createBloke(BlokeFactory.getBlokeFactory().GOGORRA);
+	            	Bloke blokeGog = blokeFactory.createBloke("gogorra");
                     matriz[i][j] = new Gelaxka(blokeGog, false, false);
 	            }
 	            
@@ -38,25 +39,21 @@ public class Classic extends Laberinto {
                     int prob1 = r.nextInt(100); // 0-99
                     // 40% probabilitate bloke biguna
                     if (prob1 < 40) { 
-                    	Bloke blokeBig = blokeFactory.createBloke(BlokeFactory.getBlokeFactory().BIGUNA);
+                    	Bloke blokeBig = blokeFactory.createBloke("biguna");
                         matriz[i][j] = new Gelaxka(blokeBig, false, false);
                         gehituSuntsigarri();
                     }
-                    //cuando el etsaia este hecho, cambiaremos este else por el comentado debajo
-                    else {
-                    	matriz[i][j] = new Gelaxka(null, false, false);
-                    }
-                    /*
                     else {
                         int prob2 = r.nextInt(100);
                         // 10% probabilitate etsaientzat
-                        if (prob2 > 90 && getEtsaiak() < 6) {
-                            matriz[i][j] = new Gelaxka(new Etsaia(), false, true);
-                            gehituEtsaia();
-                        } else {
+                        if (prob2 > 90 && jokoa.getEtsaiKop() < 6) {
+                            matriz[i][j] = new Gelaxka(null, false, true);
+                            jokoa.gehituEtsaia(new Etsaia(i,j));
+                        }
+                        else {
                             matriz[i][j] = new Gelaxka(null, false, false);
                         }
-                    }*/
+                    }
                 }
 	        }
 	    }
